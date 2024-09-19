50f88dc7dfa1956b5a24fbfa6392ee7ad526665d
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-09-19 10:16:16 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd /home/ubuntu/
-docker run -it -d -p 3000:3000 node
+echo "Logging into Amazon ECR..."
+aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <account_id>.dkr.ecr.<region>.amazonaws.com
+echo "Pulling the latest Docker image..."
+docker pull 148761652675.dkr.ecr.ap-southeast-2.amazonaws.com/test-node-app:latest
+#echo "Stopping the existing Docker container..."
+#docker stop $(docker ps -a -q --filter ancestor=<account_id>.dkr.ecr.<region>.amazonaws.com/my-node-app:latest) || true
+#docker rm $(docker ps -a -q --filter ancestor=<account_id>.dkr.ecr.<region>.amazonaws.com/my-node-app:latest) || true
+
+echo "Running the latest Docker container..."
+docker run -d -p 3000:3000 148761652675.dkr.ecr.ap-southeast-2.amazonaws.com/test-node-app:latest
